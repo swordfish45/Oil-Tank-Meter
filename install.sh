8d88436502972c37d782ee7a3a9079a35f7efc4a
swordfish45 <swordfish45@gmail.com>
2017-12-15 18:30:56 -0500

    added webgui

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 mkdir -p /usr/lib/cgi-bin/
-cp monitor.py /usr/lib/cgi-bin/.
-cp webgui.py /usr/lib/cgi-bin/.
-cp fuellog.db /usr/lib/cgi-bin/.
-chmod +x /usr/lib/cgi-bin/monitor.py
-chmod +x /usr/lib/cgi-bin/fuellog.db
-chmod +x /usr/lib/cgi-bin/webgui.py
-chown www-data:www-data /usr/lib/cgi-bin/monitor.py
-chown www-data:www-data /usr/lib/cgi-bin/fuellog.db
-chown www-data:www-data /usr/lib/cgi-bin/webgui.py
+cp *.py /usr/lib/cgi-bin/.
+cp fuellog.db /var/www/.
+chmod +x /usr/lib/cgi-bin/*.py
+chmod +x /var/www/fuellog.db
+chown www-data:www-data /usr/lib/cgi-bin/*.py
+chown www-data:www-data /var/www/fuellog.db
